6690dc01587584390476f02ab737b9d2b0f58364
mora-cier <mcier@mail.austral.edu.ar>
2025-05-12 15:52:48 -0300

    Mora lists.py

--- lists.py
+++ lists.py
@@ -1,17 +1,27 @@
 def remove_elements(list_to_remove_elements):
     len_elem = len(list_to_remove_elements)
+    
     if len_elem >= 6 :
         del list_to_remove_elements[4:6]
         del list_to_remove_elements[0]
         return list_to_remove_elements
-    elif len_elem >= 1 and len_elem <= 5:
+
+    elif len_elem == 5 :
+        del list_to_remove_elements[4]
+        del list_to_remove_elements[0]
+        return list_to_remove_elements
+
+    elif len_elem >= 1 and len_elem <= 5 :
         del list_to_remove_elements[0]
         del list_to_remove_elements[4:]
         return list_to_remove_elements
-    elif len_elem >= 1 and len_elem < 4:
+    
+    elif len_elem >= 1 and len_elem < 4 :
         del list_to_remove_elements[0]
-    elif len_elem < 1:
+    
+    elif len_elem < 1 :
         return list_to_remove_elements
+
 
 def add_elements(list_to_add_elements):
     list_to_add_elements.insert(0, "Pink")
@@ -30,11 +40,17 @@ def check_lists(list_to_compare1, list_to_compare2):
     len_1 = len(list_to_compare1)
     len_2 = len(list_to_compare2)
     if len_1 >= 3 and len_2 >= 3:
-            list_to_compare1[2] == list_to_compare2[2]
+        list_to_compare1[2] == list_to_compare2[2]
+        return True
     else:
         return False
 
 
+def list_of_lists(list_of_lists_to_modify):
+    list_of_lists_to_modify[0] = list_of_lists_to_modify[0][2:]
+    list_of_lists_to_modify[1] = list_of_lists_to_modify[1][1:4]
+    ist_of_lists_to_modify[2] = list_of_lists_to_modify[2][2:1:-1]
+    return list_of_lists_to_modify
 def list_of_lists(list_of_lists_to_modify):
     list_of_lists_to_modify[0] = list_of_lists_to_modify[0][2:]
     list_of_lists_to_modify[1] = list_of_lists_to_modify[1][1:4]
